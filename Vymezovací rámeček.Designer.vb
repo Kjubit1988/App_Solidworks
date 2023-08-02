604f823bffea3742566ffef0ac6ac5eb18e8f8ad
Kjubit1988 <141227192+Kjubit1988@users.noreply.github.com>
2023-08-02 14:17:55 +0200

    Initial commit

---
+++ Vymezovací rámeček.Designer.vb
@@ -0,0 +1,100 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        Label1 = New Label()
+        Button1 = New Button()
+        FolderBrowserDialog1 = New FolderBrowserDialog()
+        Button2 = New Button()
+        CheckBox1 = New CheckBox()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(63, 113)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(363, 23)
+        TextBox1.TabIndex = 0
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(63, 80)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(106, 15)
+        Label1.TabIndex = 1
+        Label1.Text = "Cesta k souborům:"
+        ' 
+        ' Button1
+        ' 
+        Button1.AccessibleRole = AccessibleRole.Dialog
+        Button1.Location = New Point(450, 112)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(96, 23)
+        Button1.TabIndex = 2
+        Button1.Text = "Prozkoumat"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(62, 165)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(75, 23)
+        Button2.TabIndex = 3
+        Button2.Text = "Spustit"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' CheckBox1
+        ' 
+        CheckBox1.AutoSize = True
+        CheckBox1.Location = New Point(64, 143)
+        CheckBox1.Name = "CheckBox1"
+        CheckBox1.Size = New Size(198, 19)
+        CheckBox1.TabIndex = 4
+        CheckBox1.Text = "Vymezovací rámeček viditelnost "
+        CheckBox1.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(CheckBox1)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label1)
+        Controls.Add(TextBox1)
+        Name = "Form1"
+        Text = "Macro pro vymezovací rámeček"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents FolderBrowserDialog1 As FolderBrowserDialog
+    Friend WithEvents Button2 As Button
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ Vymezovací rámeček.vb
@@ -0,0 +1,138 @@
+﻿Imports System.IO
+Imports swDocumentTypes_e = SwConst.swDocumentTypes_e
+Imports swOpenDocOptions_e = SwConst.swOpenDocOptions_e
+Imports swSaveAsOptions_e = SolidWorks.Interop.swconst.swSaveAsOptions_e
+Imports ModelView = SldWorks.ModelView
+Imports SldWorks
+Imports SolidWorks.Interop.swconst
+Imports Aspose.Slides.MathText
+
+Public Class Form1
+    Private Const swOpenDocOptions_Silent As SolidWorks.Interop.swconst.swOpenDocOptions_e = swOpenDocOptions_e.swOpenDocOptions_Silent
+    Private swModel As SldWorks.ModelDoc2
+    Private swSketch As SldWorks.Sketch
+    Private swBoundingBox As Object
+    Private swApp As SldWorks.SldWorks
+    Private swModelView As ModelView
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim dialog As New FolderBrowserDialog
+
+        With dialog
+            .Description = "Vyberte adresář"
+            .ShowNewFolderButton = False
+
+            If .ShowDialog() = DialogResult.OK Then
+                TextBox1.Text = .SelectedPath
+            End If
+        End With
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim strFolderPath As String = TextBox1.Text
+
+        If Directory.Exists(strFolderPath) Then
+            swApp = New SldWorks.SldWorks
+            swApp.OpenDoc6("", swDocumentTypes_e.swDocNONE, swOpenDocOptions_Silent, "", 0, 0)
+            swApp.Visible = True
+
+            ' Získání seznamu souborů ve vybraném adresáři
+            Dim fileNames As String() = Directory.GetFiles(strFolderPath, "*.SLDPRT")
+
+            ' Procházení a zpracování jednotlivých souborů
+            For Each fileName As String In fileNames
+                Dim swModel As SldWorks.ModelDoc2 = swApp.OpenDoc6(fileName, swDocumentTypes_e.swDocPART, swOpenDocOptions_Silent, "", 0, 0)
+                Dim swSketch As SolidWorks.Interop.sldworks.Sketch = Nothing
+                Dim swBoundingBox As Object = Nothing
+
+
+                'Zde můžete provést další zpracování souboru, např. manipulaci s modelem
+
+                swBoundingBox = swModel.FeatureManager.InsertGlobalBoundingBox(SwConst.swGlobalBoundingBoxFitOptions_e.swBoundingBoxType_BestFit, False, False, 0)
+                swModel.ClearSelection2(True)
+
+
+
+
+                'Tento blok je pro check button -skrytí nebo zobrazení vymezovacího rámečku
+                Dim ext As SldWorks.ModelDocExtension = swModel.Extension
+                Dim sketch As SldWorks.ModelDoc2
+                Dim featMgr As SldWorks.FeatureManager = swModel.FeatureManager
+
+
+
+
+                Dim boolstatus As Boolean
+                sketch = swApp.ActiveDoc
+
+
+                If CheckBox1.Checked Then
+                    boolstatus = ext.SelectByID2("Bounding Box", "BBOXSKETCH", 0, 0, 0, False, 0, Nothing, 0)
+                    sketch.UnblankSketch()
+                Else
+                    boolstatus = ext.SelectByID2("Bounding Box", "BBOXSKETCH", 0, 0, 0, False, 0, Nothing, 0)
+                    sketch.BlankSketch()
+
+                End If
+
+                swModel.ClearSelection2(True)
+
+                Dim partDoc As SldWorks.PartDoc = CType(swModel, SldWorks.PartDoc)
+
+                ' Select the bounding box sketch
+                boolstatus = ext.SelectByID2("Bounding Box", "BBOXSKETCH", 0, 0, 0, False, 0, Nothing, 0)
+
+                Dim boxMinPoint As MathPoint = Nothing
+                Dim boxMaxPoint As MathPoint = Nothing
+                Dim v = partDoc.GetPartBox(boxMinPoint, boxMaxPoint)
+
+                Dim bbox As Double() = {boxMinPoint.X, boxMinPoint.Y, boxMinPoint.Z, boxMaxPoint.X, boxMaxPoint.Y, boxMaxPoint.Z}
+
+                ' Concatenate the dimensions with "x" between them
+                Dim dimensionsString As String = String.Join("x", bbox)
+
+                ' Set the custom property with the concatenated dimensions
+                swModel.Extension.CustomPropertyManager("").Add3("BoundingBoxDimensions", swCustomInfoType_e.swCustomInfoText, dimensionsString, swCustomPropertyAddOption_e.swCustomPropertyReplaceValue)
+
+                ' ... (your existing code)
+                Dim lErrors As Long
+                Dim lwarnings As Long
+
+                swModel.Save3(swSaveAsOptions_e.swSaveAsOptions_Silent, lErrors, lwarnings)
+                'Error
+                Debug.Print("Errors as defined in swFileSaveError_e: " & lErrors)
+                ' Warnings
+                Debug.Print("Warnings as defined in swFileSaveWarning_e: " & lwarnings)
+                'swModel.Close()
+                swApp.QuitDoc(swModel.GetTitle())
+
+
+            Next
+
+            ' Ukončení instance SOLIDWORKS,
+
+            swApp.ExitApp()
+            swApp = Nothing
+            'Oznámení o uspěšném ukončení
+            MessageBox.Show("Úspešně dokončeno")
+        Else
+            MessageBox.Show("Zadejte prosím platný adresář.", "Chyba", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End If
+    End Sub
+
+    Private Sub CheckBox1_CheckedChanged(sender As Object, e As EventArgs) Handles CheckBox1.CheckedChanged
+
+        'If swModel IsNot Nothing AndAlso swBoundingBox IsNot Nothing Then
+        'If CheckBox1.Checked Then
+        'swBoundingBox.Visible = True
+
+        ' Else
+        'swBoundingBox.Visible = False
+
+        '  End If
+
+        'swModel.GraphicsRedraw2()
+        ' End If 
+    End Sub
+End Class
+
